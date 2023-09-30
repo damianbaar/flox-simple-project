7519c5282963b3993ecc6edd3f94904b66345856
Damian Baar <damian@cloudprizm.io>
2023-09-30 18:32:59 +0200

    a bit more

--- pkgs/flox-simple-project/default.nix
+++ pkgs/flox-simple-project/default.nix
@@ -4,12 +4,13 @@
 }:
 python3Packages.buildPythonPackage {
   pname = "flox-simple-project";
-  version = "0.0.2";
+  version = "0.0.3";
   src = self;
   PIP_DISABLE_PIP_VERSION_CHECK = 1;
   # Add Python modules needed by your package here
   propagatedBuildInputs = with python3Packages; [
     requests
+    boto3
   ];
   meta.description = "an example flox package";
   meta.mainProgram = "flox-simple-project";
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="flox-simple-project",
-    version="0.1",
+    version="0.3",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'hello = hello:hello',
+            'hello_world = hello:hello',
         ],
     }
 )
